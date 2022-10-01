bfd58d5adb00f50ee0a15b357041c51f7b15c479
HadiElnemr <hadi.elnemr@gmail.com>
2022-10-01 16:39:03 +0200

    refactor: check if card is not a content, added a comment

--- src/course.py
+++ src/course.py
@@ -56,7 +56,8 @@ class Course:
         files_body = self.course_soup.find_all(class_="card-body")
 
         for item in files_body:
-            if item.find('strong') == None:
+            """check if card is not a course content, useful for 'Filter weeks' card"""
+            if item.find('strong') is None:
                 continue
             self.files.append((CMSFile(soup=item, course_path=course_path)))
 
