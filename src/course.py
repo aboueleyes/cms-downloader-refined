ec9cada93d25e5eb332f8e92d9fed9cf5a3d3661
HadiElnemr <hadi.elnemr@gmail.com>
2022-10-01 16:59:11 +0200

    refactor comment

--- src/course.py
+++ src/course.py
@@ -56,7 +56,7 @@ class Course:
         files_body = self.course_soup.find_all(class_="card-body")
 
         for item in files_body:
-            """check if card is not a course content, useful for 'Filter weeks' card"""
+            # check if the card is not a course content, useful for `Filter weeks` card
             if item.find('strong') is None:
                 continue
             self.files.append((CMSFile(soup=item, course_path=course_path)))
